b978bc8119d6dde7182edd8667b8c30f0c4c2511
Pravar <pravarkhandelwal47@gmail.com>
2026-03-08 16:32:18 +0530

    Final App

--- app/src/main/java/com/example/registrationform/MainActivity.java
+++ app/src/main/java/com/example/registrationform/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.registrationform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void btnRegister(View view){
+        TextView txtFirstName = findViewById(R.id.txtViewFirstName);
+        TextView txtLastName = findViewById(R.id.txtViewLastName);
+        TextView txtEmail = findViewById(R.id.txtViewEmail);
+
+        EditText edtTxtFirstName = findViewById(R.id.edtTextFirstName);
+        EditText edtTxtLastName = findViewById(R.id.editTxtLastName);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        txtFirstName.setText(edtTxtFirstName.getText().toString());
+        txtLastName.setText(edtTxtLastName.getText().toString());
+        txtEmail.setText(edtTxtEmail.getText().toString());
     }
 }
